8cffcb103f78fd16de3803b1008bf451b7d22c29
Yousif <yousef2000khalid@gmail.com>
2021-02-01 20:53:18 +1100

    Changed webrtc usage from target to variables

--- files/cmake/Install.cmake
+++ files/cmake/Install.cmake
@@ -36,75 +36,64 @@ set(WEBRTC_BUILD_ROOT ${CMAKE_BINARY_DIR}/src/out/${CMAKE_BUILD_TYPE})
 
 # Getting the object files required for the library
 
-set(OBJ_EXT "o")
+#set(OBJ_EXT "o")
 
-if (WIN32)
-	set(OBJ_EXT "obj")
-endif()
+#if (WIN32)
+#	set(OBJ_EXT "obj")
+#endif()
 
-file(
-	GLOB_RECURSE OBJ_FILES
-# 	${WEBRTC_BUILD_ROOT}/obj/testing/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/third_party/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/api/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/rtc_base/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/modules/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/pc/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/system_wrappers/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/common_video/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/common_audio/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/media/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/call/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/p2p/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/logging/*.${OBJ_EXT}
-# 	${WEBRTC_BUILD_ROOT}/obj/audio/*.${OBJ_EXT}
-	${WEBRTC_BUILD_ROOT}/obj/*.${OBJ_EXT}
-)
+ #file(
+ #	GLOB_RECURSE OBJ_FILES
+ #	${WEBRTC_BUILD_ROOT}/obj/*.${OBJ_EXT}
+ #)
 
 # Exclude protobuf files because we externally supply them
-file(
-	GLOB_RECURSE OBJ_EXCLUDES
-	${WEBRTC_BUILD_ROOT}/obj/third_party/protobuf/*.${OBJ_EXT}
-	#${WEBRTC_BUILD_ROOT}/obj/api/audio_codecs/builtin_audio_decoder_factory/builtin_audio_decoder_factory.o
-)
+#file(
+#	GLOB_RECURSE OBJ_EXCLUDES
+#	${WEBRTC_BUILD_ROOT}/obj/third_party/protobuf/*.${OBJ_EXT}
+#	#${WEBRTC_BUILD_ROOT}/obj/api/audio_codecs/builtin_audio_decoder_factory/builtin_audio_decoder_factory.o
+#)
 
-list(LENGTH OBJ_EXCLUDES OBJ_EXCLUDES_LEN)
+#list(LENGTH OBJ_EXCLUDES OBJ_EXCLUDES_LEN)
 
-if (${OBJ_EXCLUDES_LEN} GREATER "0")
-	list(REMOVE_ITEM OBJ_FILES ${OBJ_EXCLUDES})
-endif()
+#if (${OBJ_EXCLUDES_LEN} GREATER "0")
+#	list(REMOVE_ITEM OBJ_FILES ${OBJ_EXCLUDES})
+#endif()
+#
+#set(OBJECT_FILES "")
+#
+#foreach(ofile ${OBJ_FILES})
+#	string(FIND ${ofile} "test" IS_TEST)
+#	if (${IS_TEST} EQUAL -1)
+#		# the object file is not a test
+#		set(OBJECT_FILES ${OBJECT_FILES} ${ofile})
+#	endif()
+#endforeach()
+#
+#set_source_files_properties(${OBJECT_FILES}
+#	PROPERTIES
+#	EXTERNAL_OBJECT true
+#	GENERATED true
+#)
+#
+## Create the library with these object files
+#add_library(webrtc STATIC ${OBJECT_FILES})
+#add_dependencies(webrtc webrtc_build)
+#
+#set_target_properties(
+#	webrtc PROPERTIES
+#	LINKER_LANGUAGE C
+#)
 
-set(OBJECT_FILES "")
 
-foreach(ofile ${OBJ_FILES})
-	string(FIND ${ofile} "test" IS_TEST)
-	if (${IS_TEST} EQUAL -1)
-		# the object file is not a test
-		set(OBJECT_FILES ${OBJECT_FILES} ${ofile})
-	endif()
-endforeach()
-
-set_source_files_properties(${OBJECT_FILES}
-	PROPERTIES
-	EXTERNAL_OBJECT true
-	GENERATED true
-)
-
-# Create the library with these object files
-add_library(webrtc STATIC ${OBJECT_FILES})
-add_dependencies(webrtc webrtc_build)
-
-set_target_properties(
-	webrtc PROPERTIES
-	LINKER_LANGUAGE C
-)
-
-# Modify the extension based on OS
+#Modify the extension based on OS
 #set(WEBRTC_LIB_EXT "a")
-
+#
 #if (WIN32)
 #	set(WEBRTC_LIB_EXT "lib")
 #endif()
+
+set(WEBRTC_LIBRARY ${WEBRTC_BUILD_ROOT}/obj/libwebrtc.a)
 
 #file(GLOB_RECURSE
 #	WEBRTC_LIBS
@@ -131,16 +120,16 @@ set_target_properties(
 #		DESTINATION ${CMAKE_INSTALL_LIB_DIR}
 #		COMPONENT Libraries
 #	)
-#
 #endforeach()
 
 #message(STATUS "${WEBRTC_LIBRARIES}")
 
-#install(
-#	FILES ${WEBRTC_LIBRARY}
-#	DESTINATION ${CMAKE_INSTALL_LIB_DIR}
-#	COMPONENT Libraries
-#)
+install(
+	FILES ${WEBRTC_LIBRARY}
+	DESTINATION ${CMAKE_INSTALL_LIB_DIR}
+	COMPONENT Libraries
+)
+
 # Installing header files
 
 file(
@@ -173,25 +162,25 @@ foreach(f ${HEADER_FILES})
 	)
 endforeach()
 
-install(TARGETS webrtc
-	EXPORT WebrtcTargets
- 	LIBRARY DESTINATION lib
- 	ARCHIVE DESTINATION lib
- 	RUNTIME DESTINATION bin
- 	INCLUDES DESTINATION include
-)
+#install(TARGETS webrtc
+#	EXPORT WebrtcTargets
+# 	LIBRARY DESTINATION lib
+# 	ARCHIVE DESTINATION lib
+# 	RUNTIME DESTINATION bin
+# 	INCLUDES DESTINATION include
+#)
+# 
+#install(
+# 	EXPORT WebrtcTargets
+# 	FILE WebrtcTargets.cmake
+# 	NAMESPACE ${PROJECT_NAME}::
+# 	DESTINATION lib/cmake/webrtc
+#)
  
-install(
- 	EXPORT WebrtcTargets
- 	FILE WebrtcTargets.cmake
- 	NAMESPACE ${PROJECT_NAME}::
- 	DESTINATION lib/cmake/webrtc
-)
- 
-configure_package_config_file(
+configure_file(
  	${CMAKE_CURRENT_SOURCE_DIR}/Config.cmake.in
  	"${CMAKE_CURRENT_BINARY_DIR}/WebRTCConfig.cmake"
-	INSTALL_DESTINATION ${CMAKE_INSTALL_LIB_DIR}/cmake/webrtc
+	@ONLY
 )
 
 write_basic_package_version_file(
